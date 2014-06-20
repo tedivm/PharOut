c9b8f79bc9497614977858265e2061bac601ad07
Robert Hafner <tedivm@tedivm.com>
2014-06-20 14:07:59 +0200

    Setup command base

--- src/PharOut/Commands/Base.php
+++ src/PharOut/Commands/Base.php
@@ -3,25 +3,89 @@
 namespace PharOut\Commands;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 abstract class Base extends Command {
 
     protected $name;
     protected $description;
     protected $help;
+    protected $options = array();
+    protected $arguments = array();
+
+    protected $metaDoc;
 
     protected function configure()
     {
-        $this->setName($this->name)
-            ->setDescription($this->description)
-            ->setDefinition($this->getOptions())
+        $classname = substr(__CLASS__, strrpos(__CLASS__, '\\') - 1);
+        $metaPath = __DIR__ . '/' . $classname . '.json';
+        if(file_exists($metaPath)) {
+            $this->metaDoc = json_decode($metaPath);
+        }
+
+
+        $this->setName($this->name);
+        $this->setDescription($this->description);
+
+        $this->setName($this->getName())
+            ->setDescription($this->getDescription())
             ->setHelp($this->help);
+
+
+        foreach($this->getArguments() as $argument) {
+            $this->addArgument(
+                $argument[0], // name
+                isset($argument[1]) ? $argument[1] : null, // mode
+                isset($argument[2]) ? $argument[2] : '', // description
+                isset($argument[3]) ? $argument[3] : null // default
+            );
+        }
+
+        foreach($this->getOptions() as $option) {
+            $this->addOption(
+                $option[0], // name
+                isset($option[1]) ? $option[1] : null, // shortcut
+                isset($option[2]) ? $option[2] : null, // mode
+                isset($option[3]) ? $option[3] : '', // description
+                isset($option[4]) ? $option[4] : null // default
+            );
+        }
     }
 
-    abstract protected function getOptions();
+    protected function getArguments()
+    {
+        $arguments = $this->arguments;
+
+        if(is_array($this->metaDoc) && isset($this->metaDoc['arguments'])) {
+            foreach($this->metaDoc['arguments'] as $metaArgument) {
+                $arguments[] = array(
+                    $metaArgument['name'], // name
+                    isset($metaArgument['mode']) ? $metaArgument['mode'] : null, // mode
+                    isset($metaArgument['description']) ? $metaArgument['description'] : '', // description
+                    isset($metaArgument['default']) ? $metaArgument['default'] : null // default
+                );
+            }
+        }
+
+        return $arguments;
+    }
+
+    protected function getOptions()
+    {
+        $options = $this->options;
+
+        if(is_array($this->metaDoc) && isset($this->metaDoc['options'])) {
+            foreach($this->metaDoc['options'] as $metaOption) {
+                $options[] = array(
+                    $metaOption['name'], // name
+                    isset($metaOption['shortcut']) ? $metaOption['shortcut'] : null, // mode
+                    isset($metaOption['mode']) ? $metaOption['mode'] : null, // mode
+                    isset($metaOption['description']) ? $metaOption['description'] : '', // description
+                    isset($metaOption['default']) ? $metaOption['default'] : null // default
+                );
+            }
+        }
+
+        return $options;
+    }
+
 }
--- src/PharOut/Commands/Build.php
+++ src/PharOut/Commands/Build.php
@@ -2,11 +2,8 @@
 
 namespace PharOut\Commands;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class Build extends base {
@@ -14,15 +11,7 @@ class Build extends base {
     protected $name = 'build';
     protected $description = 'Description';
     protected $help = <<<EOT
-
 EOT;
-
-    protected function getOptions()
-    {
-        // array of InputOption class
-        return array(
-        );
-    }
 
 
     protected function execute(InputInterface $input, OutputInterface $output)
